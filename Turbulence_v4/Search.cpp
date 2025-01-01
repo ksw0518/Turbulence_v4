21c34cbe65816be3c9d72f2c4ebed9b2ce193242
ksw0518 <cs0503115@gmail.com>
2025-01-01 18:13:31 +0900

    tt_fix

--- Turbulence_v4/Search.cpp
+++ Turbulence_v4/Search.cpp
@@ -1024,12 +1024,12 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 			}
 			else if (ttEntry.node_type == AlphaFlag && ttEntry.score <= alpha)
 			{
-				return ttEntry.score;
+				return alpha;
 				//alpha = ttEntry.score;
 			}
-			else if (ttEntry.node_type == BetaFlag && ttEntry.score >= beta )
+			else if (ttEntry.node_type == BetaFlag && ttEntry.score >= beta)
 			{
-				return ttEntry.score;
+				return beta;
 			}
 
 			//if (alpha >= beta)
