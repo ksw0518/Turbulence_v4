c8c4f72dd0afc6cb2fbfa8ad04eece851220c31f
ksw0518 <cs0503115@gmail.com>
2025-01-22 23:06:51 +0900

    bench 2164588

--- Turbulence_v4/Search.cpp
+++ Turbulence_v4/Search.cpp
@@ -1513,9 +1513,9 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 			{
 				reduction--;
 			}
-			if (!improving)
+			if (improving)
 			{
-				reduction++;
+				reduction--;
 			}
 			//asdf
 
