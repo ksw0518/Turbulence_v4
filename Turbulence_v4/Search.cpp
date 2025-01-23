f69843eb50acb13d0cf0495cd3ee26f72255ab2c
ksw0518 <cs0503115@gmail.com>
2025-01-23 22:40:09 +0900

    reduce more on quiet moves

--- Turbulence_v4/Search.cpp
+++ Turbulence_v4/Search.cpp
@@ -1507,6 +1507,10 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 			{
 				reduction--;
 			}
+			if (isQuiet)
+			{
+				reduction++;
+			}
 			//asdf
 
 			//if (beta - alpha >= 1) //reduce less on pv nodes
