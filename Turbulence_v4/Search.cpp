860e8171bb815178f130d179760288198e19436b
ksw0518 <cs0503115@gmail.com>
2025-01-24 17:53:50 +0900

    bench 2012019

--- Turbulence_v4/Search.cpp
+++ Turbulence_v4/Search.cpp
@@ -1531,6 +1531,10 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 			{
 				reduction--;
 			}
+			if (historyScore < -25 * depth)
+			{
+				reduction++;
+			}
 			//asdf
 
 			//if (beta - alpha >= 1) //reduce less on pv nodes
