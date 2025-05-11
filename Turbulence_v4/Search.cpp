b955301695d184fec9544919b9d7d1d2c41adcf5
ksw0518 <cs0503115@gmail.com>
2025-05-11 16:56:09 +0900

    bench 4170865

--- Turbulence_v4/Search.cpp
+++ Turbulence_v4/Search.cpp
@@ -989,7 +989,7 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 	int canPrune = !isInCheck && !isPvNode;
 	//RFP
 	//If static evaluation + margin still doesn't improve alpha, prune the node
-	if (!isSingularSearch && depth < 5 && canPrune)
+	if (!tt_pv && !isSingularSearch && depth < 5 && canPrune)
 	{
 		int rfpMargin;
 		if (improving)
