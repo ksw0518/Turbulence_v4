4fdde25eb08d1e31fcbc6373aa963100034de0a3
ksw0518 <cs0503115@gmail.com>
2025-05-18 20:26:28 +0900

    bench 3623116

--- Turbulence_v4/Search.cpp
+++ Turbulence_v4/Search.cpp
@@ -1187,7 +1187,7 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 				}
 			}
 		}
-
+		AccumulatorPair ueacc = board.accumulator;
 		MakeMove(board, move);
 
 		data.searchNodeCount++;
@@ -1265,6 +1265,39 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 				extensions -= 2;
 			}
 
+			if (get_piece(move.Piece, White) == K)//king has moved
+			{
+				if (move.From <= 3)//king was left before
+				{
+					if (move.To >= 4)//king moved to right 
+					{
+						//fully refresh the stm accumulator, and change that to start mirroring
+						if (board.side == White)
+						{
+							resetWhiteAccumulator(board, board.accumulator, true);
+						}
+						else
+						{
+							resetBlackAccumulator(board, board.accumulator, true);
+						}
+					}
+				}
+				else//king was right before
+				{
+					if (move.To <= 3)//king moved to left 
+					{
+						//fully refresh the stm accumulator, and change that to stop mirroring
+						if (board.side == White)
+						{
+							resetWhiteAccumulator(board, board.accumulator, false);
+						}
+						else
+						{
+							resetBlackAccumulator(board, board.accumulator, false);
+						}
+					}
+				}
+			}
 			MakeMove(board, move);
 			data.ply++;
 		}
