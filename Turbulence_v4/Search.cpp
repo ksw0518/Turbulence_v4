5446b43e28663911748c58680363e3d4280e775d
ksw0518 <cs0503115@gmail.com>
2025-05-22 23:26:35 +0900

    bench 3626599

--- Turbulence_v4/Search.cpp
+++ Turbulence_v4/Search.cpp
@@ -1173,6 +1173,11 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 		{
 			continue;
 		}
+		bool isNotMated = alpha > -49000 + 99;
+		if (isQuiet && depth <= 7 && !isPvNode && staticEval + 200 * depth <= alpha && isNotMated)
+		{
+			continue;
+		}
 		int seeThreshold = isQuiet ? quietSEEMargin : noisySEEMargin; 
 		if (data.ply != 0 && depth <= MAX_PVS_SEE_DEPTH)
 		{
@@ -1182,7 +1187,6 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 				continue;
 			}
 		}
-		bool isNotMated = alpha > -49000 + 99;
 
 		int main_history = data.mainHistory[board.side][move.From][move.To][Get_bit(oppThreats, move.From)][Get_bit(oppThreats, move.To)];
 		int conthist = getContinuationHistoryScore(move, data) * 2;
