8817f3ba0e64b9e8849222617cce7b6de16060e2
ksw0518 <cs0503115@gmail.com>
2025-05-24 16:59:56 +0900

    bench 3663895

--- Turbulence_v4/UCI.cpp
+++ Turbulence_v4/UCI.cpp
@@ -391,7 +391,7 @@ void ProcessUCI(std::string input, ThreadData& data, ThreadData* data_heap)
     if (main_command == "uci")
     {
         
-        std::cout << "id name Turbulence_v4 v0.0.6" << "\n";;
+        std::cout << "id name Turbulence_v4 v0.0.7" << "\n";;
         std::cout << "id author ksw0518" << "\n";;
         std::cout << "\n";
         std::cout << "option name Threads type spin default 1 min 1 max 1\n";
