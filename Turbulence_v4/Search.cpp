fcf19f1b5f3c686ef05b2030b771e31c27492a2e
ksw0518 <cs0503115@gmail.com>
2025-06-28 12:07:43 +0900

    bench 2952136

--- Turbulence_v4/Search.cpp
+++ Turbulence_v4/Search.cpp
@@ -1384,6 +1384,10 @@ static inline int Negamax(Board& board, int depth, int alpha, int beta, bool doN
 			{
 				reduction_bonus-= LMR_TTDEPTH_SUB;
 			}
+			if (searchedMoves <= 3)
+			{
+				reduction_bonus -= 700;
+			}
 			reduction_bonus /= 1024;
 			reduction += reduction_bonus;
 		}
